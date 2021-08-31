ebcd021ea7bce55e621937f4f9642a4ff9de66d3
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-09-01 01:52:45 +0200

    DbContext. Updates kommer

---
+++ pizzaBestilling/Models/DbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace pizzaBestilling.Models
+{
+    public class Kunde
+    {   [Key]
+        public int KId { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Telefonnr { get; set; }
+        public virtual List<Bestilling> Bestillinger { get; set; }
+    }
+
+    public class Bestilling
+    {
+        [Key]
+        public int BId { get; set; }
+        public string PizzaType { get; set; }
+        public string Tykkelse { get; set; }
+        public int Antall { get; set; }
+
+    }
+
+
+
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
+            Database.EnsureCreated(); 
+        }
+
+        public DbSet<Kunde> Kunder { get; set; }
+        public DbSet<Bestilling> Bestillinger { get; set; }
+
+     /*   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies(optionsBuilder, true);
+        }*/
+
+    }
+}
