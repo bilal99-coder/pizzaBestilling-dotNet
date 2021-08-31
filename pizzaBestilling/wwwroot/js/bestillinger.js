dbba661c164461dfe8874334c4bdd8386a5abafb
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-08-31 14:45:22 +0200

    Added filen bestillnger.html som skal ha oversikt over alle bestillger.

---
+++ pizzaBestilling/wwwroot/js/bestillinger.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $.get("pizza/HentAlle", function (bestillinger) {
+        formaterBestillinger(bestillinger);
+    });
+});
+
+function formaterBestillinger(bestillinger) {
+    let ut = "<table class="table table-striped">" +
+        "<tr>" +
+        "<th>Navn</th><th>Adresse</th><th>Telefonnr</th>PizzaType</th>" +
+        "<th>Tykkelse</th><th>Antall</th>" 
+}
